b808a95710679934c54dfdca4107a6b2406c362f
GuilhermeAugustini <guilherme.maugustini@gmail.com>
2020-08-15 15:16:54 -0300

    first commit

---
+++ src/components/Board/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import  { loadLists } from '../../services/api';
+
+import List from '../List';
+
+import { Container } from './style';
+
+const lists = loadLists();
+
+export default function Board() {
+    return (
+        <Container>
+            {lists.map( list => <List key={list.title} data={list} />)}
+        </Container>
+        
+    );
+}
