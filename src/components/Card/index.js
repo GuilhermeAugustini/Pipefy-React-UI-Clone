28f3e1c269a392c06b5585c5a12e88ee3cd3f500
GuilhermeAugustini <guilherme.maugustini@gmail.com>
2020-08-16 21:35:20 -0300

    Add drop in card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -11,6 +11,33 @@ export default function Card({ data }) {
             isDragging: monitor.isDragging(),
         }),
     });
+    const [, dropRef ] = useDrop({
+        accept: 'CARD',
+        hover(item, monitor) {
+            const draggedIndex = item.index;
+            const targetIndex = index;
+
+            if (draggedIndex === targetIndex){
+                return;
+            }
+
+            const targetSize = ref.current.getBoundingClientRect()
+            const targertCenter = (targetSize.bottom - targetSize.top) /2;
+
+            const draggedOffset = monitor.getClientOffset();
+            const draggedTop = draggefOffset.y - targetSize.top;
+
+            if(draggedIndex < targetIndex && draggedTop < targetcenter) {
+                return;
+            }
+
+            if (draggedIndex > targetIndex && draggedTop > targetCenter) {
+                return;
+            }
+        }
+    })
+
+
     return (
         <Container ref={dragRef} isDragging={isDragging}>
            <header>
